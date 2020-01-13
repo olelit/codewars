0216a269bafe9cd727e298bad44e6e006d4c178e
olelit <o.litasov@imedia.by>
2020-01-13 16:37:48 +0300

    going

--- going.py
+++ going.py
@@ -1,15 +1,12 @@
-from decimal import *
-
 def factorial(n):
     if(n <= 1):
         return n
     return n * factorial(n-1)
 
 def going(n):
-    first = Decimal(1/factorial(n))
-    print(first)
-    second = Decimal(sum([factorial(i) for i in range(n)]))
-    return first * second
+    first = 1/factorial(n)
+    second = sum([factorial(i) for i in range(n)])
+    return 1 + first * second
 
 
-print(going(200))
+print(going(8))
